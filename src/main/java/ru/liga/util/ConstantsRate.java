f43f5be350e5a95e1dc6fe53cf97137cf89efa51
Guzime <40675616+Guzime@users.noreply.github.com>
2022-06-27 12:45:26 +0300

    Little fix out_pattern_dt

--- src/main/java/ru/liga/util/ConstantsRate.java
+++ src/main/java/ru/liga/util/ConstantsRate.java
@@ -5,6 +5,6 @@ public class ConstantsRate {
     public static final String PATH_TRY = "src/main/resources/RC_F01_06_2002_T17_06_2022_TRY.csv";
     public static final String PATH_EUR = "src/main/resources/RC_F01_06_2002_T17_06_2022_EUR.csv";
     public static final String IN_PATTERN_DT = "dd.MM.yyyy";
-    public static final String OUT_PATTERN_DT = "dd.MM.yyyy";
+    public static final String OUT_PATTERN_DT = "EE dd.MM.yyyy";
     public static final int DAYS_OF_RATE = 7;
 }
