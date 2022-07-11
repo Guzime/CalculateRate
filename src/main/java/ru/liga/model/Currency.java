f24368f84347a01e1effad136ef9af4c9fc4184f
Guzime <40675616+Guzime@users.noreply.github.com>
2022-07-11 18:22:44 +0300

    addCurrency

---
+++ src/main/java/ru/liga/model/Currency.java
@@ -0,0 +1,19 @@
+package ru.liga.model;
+
+public enum Currency {
+    USD("src/main/resources/csv/USD_F01_02_2005_T05_03_2022.csv"),
+    TRY("src/main/resources/csv/TRY_F01_02_2005_T05_03_2022.csv"),
+    EUR("src/main/resources/csv/EUR_F01_02_2005_T05_03_2022.csv"),
+    BGN("src/main/resources/csv/BGN_F01_02_2005_T05_03_2022.csv"),
+    AMD("src/main/resources/csv/AMD_F01_02_2005_T05_03_2022.csv");
+    private String path;
+
+    Currency(String path) {
+        this.path = path;
+    }
+
+    public String getPath() {
+        return path;
+    }
+
+}
